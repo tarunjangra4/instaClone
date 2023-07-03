1d94c4080fb29697ed0d31f73850f5bcdd5d9cf2
Tarun Jangra <tarun.jangra@fincity.com>
2023-07-03 18:13:06 +0530

    story viewer component created

---
+++ src/components/StoryComponents/StoryViewer.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Progressbar from "./Progressbar";
+
+const StoryViewerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: black;
+`;
+
+const StoryImage = styled.img`
+  max-height: 90vh;
+  object-fit: content;
+`;
+
+const StoryViewer = ({ stories }) => {
+  const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNextStory = () => {
+    if (currentStoryIndex < stories.length - 1) {
+      setCurrentStoryIndex(currentStoryIndex + 1);
+      // setActiveIndex(activeIndex + 1);
+    } else if (currentStoryIndex === stories.length - 1) {
+      setCurrentStoryIndex(0);
+      // setActiveIndex(0);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNextStory();
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentStoryIndex]);
+  return (
+    <div className="relative w-full">
+      <StoryViewerContainer>
+        <StoryImage src={stories?.[currentStoryIndex].image} />
+        <div className="absolute top-0 flex w-full">
+          {stories.map((item, index) => (
+            <Progressbar
+              duration={2000}
+              index={index}
+              key={index}
+              activeIndex={currentStoryIndex}
+              // activeIndex={activeIndex}
+            />
+          ))}
+        </div>
+      </StoryViewerContainer>
+    </div>
+  );
+};
+
+export default StoryViewer;
