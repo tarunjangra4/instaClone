0dc1ea9d70f57e1d44a78f51816e31fda09725f8
Tarun Jangra <tarun.jangra@fincity.com>
2023-07-07 14:19:57 +0530

    router added

--- src/pages/router/router.js
+++ src/pages/router/router.js
@@ -1,21 +1,37 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "../HomePage/Home";
 import Profile from "../Profile";
+import Story from "../Story/Story";
+import Auth from "../Auth/Auth";
 
 const Router = () => {
+  const location = useLocation();
   return (
-    <div className="flex">
-      <div className="w-[20%] border border-l-slate-500 pl-10">
-        <Sidebar />
-      </div>
-      <div className="w-[80%]">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
-        </Routes>
-      </div>
+    <div>
+      {location.pathname === "/login" || location.pathname === "/signup" ? (
+        <div>
+          <Routes>
+            <Route path="/signup" element={<Auth />}></Route>
+            <Route path="/login" element={<Auth />}></Route>
+          </Routes>
+        </div>
+      ) : (
+        <div className="flex">
+          <div className="w-[20%] border border-l-slate-500">
+            <Sidebar />
+          </div>
+          <div className="w-[80%]">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/story" element={<Story />}></Route>
+              <Route path="/comment/:postId" element={<Home />}></Route>
+            </Routes>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
